346501d537386015554071cc3f354ad016671a13
Yohann04 <95697982+Yohann04@users.noreply.github.com>
2024-02-22 11:57:29 +0100

    Test testCalculeJoursDeCongeDecomptesPourPlage

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -3,8 +3,11 @@ package com.ipi.jva350.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 
 public class SalarieAideADomicileTest {
     @Test
@@ -37,5 +40,24 @@ public class SalarieAideADomicileTest {
         SalarieAideADomicile travaille = new SalarieAideADomicile();
         boolean res = travaille.estHabituellementTravaille(LocalDate.now());
         Assertions.assertEquals(true, res);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'2023-12-17', '2023-12-28', 9",
+        "'2023-12-17', '2024-01-08', 17"
+    })
+    public void testCalculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedNb) {
+        //Given :
+        SalarieAideADomicile monSalarie = new SalarieAideADomicile("Alexis",
+                LocalDate.of(2023, 6, 28),
+                LocalDate.now(), 20, 2.5, 9,
+                1,8);
+        //When :
+        LinkedHashSet<LocalDate> resNb = monSalarie.calculeJoursDeCongeDecomptesPourPlage(
+                LocalDate.parse(dateDebut),
+                LocalDate.parse(dateFin));
+        //Then :
+        Assertions.assertEquals(expectedNb, resNb.size());
     }
 }
